3c57fb72fee314aa6dacfa84c3af215aab8262e0
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-05-31 01:04:09 +0700

    app.py

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-python -m src.app
+python3 -m src.app
--- src/app.py
+++ src/app.py
@@ -1,19 +1,207 @@
+from enum import Enum
+from typing import Any, Dict, Tuple
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from . import explkttns
+import src.explkttns
 
 load_dotenv()
 
 app = App()
 
+class GameState(Enum):
+    NEW = "new"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+MsgId = Tuple[str, str] # (channel_id, ts)
+data: Dict[MsgId, Any] = {}
+
 @app.command("/explkttns")
-def handle_explkttns(ack, respond, command):
+def handle_explkttns(ack, say, command):
+    ack()
     # New game
-    respond(
-        text="New game of Exploding Kittens!",
-        # ... bla bla bla
+    x = say(
+        text="New game of Exploding Kittens!"
     )
+    ts = x['ts']
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Invite someone"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Invite"
+                    },
+                    "action_id": "invite"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Start the game"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Start",
+                        "emoji": True
+                    },
+                    "action_id": "start-game"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Cancel the game"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Cancel",
+                        "emoji": True
+                    },
+                    "action_id": "cancel-game"
+                }
+            }
+        ],
+        metadata={
+            'event_type': 'pregame_actions',
+            'event_payload': {
+                'allowed_user': command['user_id'],
+            }
+        },
+        thread_ts=ts
+    )
+
+    data[(command['channel_id'], ts)] = {
+        'game_state': GameState.NEW,
+        'host': command['user_id'],
+        'players': {command['user_id']}
+    }
+
+@app.action("invite")
+def handle_invite(ack, client, action, body):
+    ack()
+
+    if body["user"]["id"] != data[(body["channel"]["id"], body["message"]["thread_ts"])]['host']:
+        client.chat_postEphemeral(
+            channel=body["channel"]["id"],
+            thread_ts=body["message"]["thread_ts"],
+            user=body["user"]["id"],
+            text="Only the host can invite players."
+        )
+        return
+
+    if data[(body["channel"]["id"], body["message"]["thread_ts"])]['game_state'] != GameState.NEW:
+        client.chat_postEphemeral(
+            channel=body["channel"]["id"],
+            thread_ts=body["message"]["thread_ts"],
+            user=body["user"]["id"],
+            text="You cannot invite players after the game has started or completed."
+        )
+        return
+
+    client.views_open(
+        trigger_id=body["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "invite_modal",
+            "title": {
+                "type": "plain_text",
+                "text": "Invite someone",
+                "emoji": True
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Invite",
+                "emoji": True
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+                "emoji": True
+            },
+            "blocks": [
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "multi_users_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select users",
+                            "emoji": True
+                        },
+                        "action_id": "invite-modal",
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": " ",
+                        "emoji": True
+                    }
+                }
+            ],
+            "private_metadata": f"{body['channel']['id']} {body['message']['thread_ts']}"
+        }
+    )
+
+@app.view("invite_modal")
+def handle_invite_modal(ack, view, body, client):
+    ack()
+    values = view["state"]["values"]
+    invite_modal = next(x["invite-modal"] for x in values.values() if "invite-modal" in x)
+    users = invite_modal["selected_users"]
+
+    if len(data[(view["private_metadata"].split()[0], view["private_metadata"].split()[1])]['players'] | users) > 5:
+        client.chat_postEphemeral(
+            channel=view["private_metadata"].split()[0],
+            thread_ts=view["private_metadata"].split()[1],
+            user=body["user"]["id"],
+            text="You can only have up to 5 players in a game."
+        )
+        return
+
+    data[(view["private_metadata"].split()[0], view["private_metadata"].split()[1])]['players'].update(users)
+    client.chat_postMessage(
+        channel=view["private_metadata"].split()[0],
+        thread_ts=view["private_metadata"].split()[1],
+        text=f"<@{body['user']['id']}> has invited {', '.join(f'<@{x}>' for x in users)}"
+    )
+
+
+@app.action("start-game")
+def handle_start_game(ack, body, client):
+    ack()
+
+    if body["user"]["id"] != data[(body["channel"]["id"], body["message"]["thread_ts"])]['host']:
+        client.chat_postEphemeral(
+            channel=body["channel"]["id"],
+            thread_ts=body["message"]["thread_ts"],
+            user=body["user"]["id"],
+            text="Only the host can start the game."
+        )
+        return
+
+    if data[(body["channel"]["id"], body["message"]["thread_ts"])]['game_state'] != GameState.NEW:
+        client.chat_postEphemeral(
+            channel=body["channel"]["id"],
+            thread_ts=body["message"]["thread_ts"],
+            user=body["user"]["id"],
+            text="You cannot start the game after it has started or completed."
+        )
+        return
+
+    ...
 
 def main():
     handler = SocketModeHandler(app)
